53c28020e2c0fad9428a7c9b6175a72a8bbf6d2d
Vitali_Varabyeu <Vitali_Varabyeu@EPBYMINW0650>
2021-12-05 15:22:41 +0300

    Add project files.

---
+++ Task 2 - Hello for task 1(v2)/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_2___Hello_for_task_1_v2_
+{
+    public class HelloMessage
+    {
+        public string? _name;
+        public HelloMessage()
+        {
+            Console.WriteLine("Please enter your name:");
+            string? name = Console.ReadLine();
+            _name = name;
+        }
+
+        public void Print()
+        { 
+            Console.WriteLine($"Hello {_name}"); 
+        }
+    }
+}
